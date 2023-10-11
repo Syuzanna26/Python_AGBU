c94cffaa4425857cbd0aea9f96b1028132912d46
Syuzanna <syuzanna26.12@gmail.com>
2023-10-11 23:15:54 +0400

    Index,Slicing

---
+++ Index, Slicing.py
@@ -0,0 +1,20 @@
+# 1.Write a program that get user input and return  every third element from inputed string.
+
+1.
+text1 = input("Please enter some word here: ")
+print(text1[::3])
+2.
+text2 = input("Please enter some word here: ")
+reverse = text2[::3]
+print(reverse)
+
+
+
+# 2.Write a program that get user input and return  every second element from inputed string from end.
+
+text1 = input("Please enter some word here: ")
+print(text1[::-2])
+
+
+
+
